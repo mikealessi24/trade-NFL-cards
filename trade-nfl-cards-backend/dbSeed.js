cdf75fa5a7cf607544b19c1e1f5de670319aa525
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2020-10-02 11:20:44 -0400

    added a login authentication route

--- trade-nfl-cards-backend/dbSeed.js
+++ trade-nfl-cards-backend/dbSeed.js
@@ -1,5 +1,6 @@
 const sql = require("mysql2/promise");
 require("dotenv").config();
+const players = require("./fbCardsDb");
 
 const pool = sql.createPool({
   host: process.env.host,
@@ -7,15 +8,32 @@ const pool = sql.createPool({
   password: process.env.password,
 });
 
-(async function testConnection() {
-  try {
-    // creating a connection
-    const conn = await pool.getConnection();
-    // do a query...
-    console.log("connection is successful", conn);
-    // manually release connection otherwise no one else can use the connection
-    conn.release();
-  } catch (error) {
-    console.log(error);
-  }
-})();
+// (async function testConnection() {
+//   try {
+//     // creating a connection
+//     const conn = await pool.getConnection();
+//     // do a query...
+//     console.log("connection is successful", conn);
+//     // manually release connection otherwise no one else can use the connection
+//     conn.release();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
+
+// async function populateDb() {
+//   try {
+//     const conn = await pool.getConnection();
+//     conn.query("USE fbcardsdb");
+//     players.map((player) => {
+//       conn.execute(
+//         `INSERT INTO cards (name,position,team,image,owner) VALUES ("${player.name}", "${player.position}", "${player.team}", "${player.image}", "${player.owner}")`
+//       );
+//     });
+//     conn.release();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// populateDb();
--- trade-nfl-cards-backend/index.js
+++ trade-nfl-cards-backend/index.js
@@ -2,6 +2,7 @@ const sql = require("mysql2/promise");
 require("dotenv").config();
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,7 @@ const pool = sql.createPool({
   password: process.env.password,
 });
 
+//create a user
 app.post("/user", async (request, response) => {
   try {
     if (!request.body.username || !request.body.password) {
@@ -31,6 +33,44 @@ app.post("/user", async (request, response) => {
 
     conn.release();
     response.status(201).send(queryResponse);
+  } catch (error) {
+    response.status(500).send(error);
+    console.log(error);
+  }
+});
+
+app.post("/login", async (request, response) => {
+  try {
+    if (!request.body.username || !request.body.password) {
+      return response.status(401).send({ message: "No username or password" });
+    }
+    const username = request.body.username;
+    const password = request.body.password;
+
+    const conn = await pool.getConnection();
+
+    const queryResponse = await conn.query(
+      `SELECT * FROM fbcardsdb.users WHERE username =?`,
+      [username]
+    );
+
+    const userFound = queryResponse[0][0];
+
+    if (!userFound) {
+      return response.status(401).send({ message: "No user found" });
+    } else {
+      if (await bcrypt.compare(password, userFound.password)) {
+        const token = jwt.sign(
+          { username: userFound.username },
+          process.env.ACCESS_TOKEN
+        );
+
+        response.status(200).send({ message: "login successful", jwt: token });
+      } else {
+        response.status(401).send({ message: "incorrect password" });
+      }
+    }
+    conn.release();
   } catch (error) {
     response.status(500).send(error);
     console.log(error);
